a2665b5319fd77ca22fa90581fe0661c795bae55
LuFeng Jiang <13688880130@139.com>
2017-10-30 13:54:19 +0800

    First program py

---
+++ first_program.py
@@ -0,0 +1,34 @@
+#-*- coding:utf-8 -*-
+
+#####################################################
+# @author   Jessus
+# @create_date 2017-7-26
+# @modify_date 2017-7-26
+# @python_version python2
+# @features    璇磆ello锛岄棶鍚嶅瓧锛屾墦鎷涘懠锛岄棶骞撮緞
+#####################################################
+
+
+print('Hello World')
+
+#print('What is your name?')
+myName=raw_input('What is your name?: ')
+
+
+if (myName == '姹熼瞾閿?):
+	print('It is good to meet you,'+ myName)
+else:
+	print('鎴戜笉璁よ瘑浣犮€?)
+	exit()
+
+#print('What is your age?')
+myPassword=input('Input your password:')
+
+if (myPassword == 'newlife@2012'):
+		print('Access granted.')
+else:
+		print('Access refused.')
+
+
+for i in range(5):
+	print('鍝堝搱鍝堝搱鍝堝搱鍝?)
